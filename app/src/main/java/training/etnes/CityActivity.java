0f03821f75c770743078d0542ee79bff7fad9861
Quentin SENTE <quentin.sente@condorcet.be>
2021-04-21 19:45:16 +0200

    first push

---
+++ app/src/main/java/training/etnes/CityActivity.java
@@ -0,0 +1,14 @@
+package training.etnes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city);
+    }
+}
